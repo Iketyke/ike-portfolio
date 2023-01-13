1a064970ea0f795e73f016e1ace0df27dcd2ab3a
Iketyke <isaac.mellad.pemberton@gmail.com>
2023-01-13 21:41:26 +0000

    add skills assets

--- src/Sections/Skills/Skills.jsx
+++ src/Sections/Skills/Skills.jsx
@@ -1,36 +1,98 @@
-import React from 'react'
-import TypingText from '../../Components/TypingText/TypingText'
-import "./Skills.css"
-import {JSLogo, CPPLogo, PyLogo, ReactLogo, ReactNLogo, CSSLogo, HTMLLogo, FirebaseLogo, ExpressLogo, PSQLLogo, JestLogo} from "../../Assets/Logos"
-const Skills = () => {
-  return (
-    <section className='App__Skills' id='skills'>
-        <h1 className='App__Skills-Title'><TypingText text={"Skills"} time={1000} delay={0}/></h1>
-        <div className='App__Skills-Languages card'>
-            <h2><TypingText text={"Languages"} time={1000} delay={0}/></h2>
-            <img src={JSLogo} alt="JS Logo" />
-            <img src={CPPLogo} alt="C++ Logo" />
-            <img src={PyLogo} alt="Python Logo" />
-        </div>
-        <div className='App__Skills-Frontend card'>
-            <h2><TypingText text={"Front End"} time={1000} delay={0}/></h2>
-            <img src={ReactLogo} alt="React Logo" />
-            <img src={ReactNLogo} alt="React Native Logo" />
-            <img src={CSSLogo} alt="CSS Logo" />
-            <img src={HTMLLogo} alt="HTML Logo" />
-        </div>
-        <div className='App__Skills-Backend card'>
-            <h2><TypingText text={"Back End"} time={1000} delay={0}/></h2>
-            <img src={FirebaseLogo} alt="Firebase Logo" />
-            <img src={ExpressLogo} alt="Express Logo" />
-            <img src={PSQLLogo} alt="PSQL Logo" />
-        </div>
-        <div className='App__Skills-Testing card'>
-            <h2><TypingText text={"Testing"} time={1000} delay={0}/></h2>
-            <img src={JestLogo} alt="Jest Logo" />
-        </div>
-    </section>
-  )
-}
+import React, { useState, useEffect, useRef } from "react";
+import TypingText from "../../Components/TypingText/TypingText";
+import "./Skills.css";
+import {
+  JSLogo,
+  CPPLogo,
+  PyLogo,
+  ReactLogo,
+  ReactNLogo,
+  CSSLogo,
+  HTMLLogo,
+  FirebaseLogo,
+  ExpressLogo,
+  PSQLLogo,
+  JestLogo,
+} from "../../Assets/Logos";
+import Button from "../../Components/Button/Button";
+import "animate.css";
 
-export default Skills
+const Skills = () => {
+    const ref = useRef(null);
+  const [revealed, setRevealed] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        console.log(revealed);
+        if (entry.isIntersecting) {
+          setTimeout(()=> {
+            setRevealed(true);
+          }, 500)
+        }
+      });
+    });
+    observer.observe(ref.current);
+    return () => observer.disconnect();
+  }, []);
+  return (
+    <section className="App__Skills" id="skills">
+      <p ref={ref} className={revealed ? "App__About-Hidden" : ""}></p>
+      {revealed ? (
+        <>
+          <h1 className="App__Skills-Title">
+            <TypingText text={"Skills"} time={1000} delay={0} />
+          </h1>
+          <div className="App__Skills-Container">
+            <div className="App__Skills-Languages card animate__animated animate__zoomIn">
+              <h2>
+                <TypingText text={"Languages"} time={1000} delay={0} />
+              </h2>
+              <img src={JSLogo} alt="JS Logo" />
+              <img src={CPPLogo} alt="C++ Logo" />
+              <img src={PyLogo} alt="Python Logo" />
+            </div>
+            <div className="App__Skills-Container2">
+              <div className="App__Skills-Frontend animate__animated animate__zoomIn card">
+                <h2>
+                  <TypingText text={"Front End"} time={1000} delay={0} />
+                </h2>
+                <div className="App__Skills-Frontend-Main">
+                  <img src={ReactLogo} alt="React Logo" />
+                  <div>
+                    <img src={ReactNLogo} alt="React Native Logo" />
+                    <img src={CSSLogo} alt="CSS Logo" />
+                    <img src={HTMLLogo} alt="HTML Logo" />
+                  </div>
+                </div>
+              </div>
+              <div className="App__Skills-BackendTesting">
+                <div className="App__Skills-Backend animate__animated animate__zoomIn card">
+                  <h2>
+                    <TypingText text={"Back End"} time={1000} delay={0} />
+                  </h2>
+                  <div className="App__Skills-Backend-Main">
+                    <div>
+                      <img src={FirebaseLogo} alt="Firebase Logo" />
+                      <img src={ExpressLogo} alt="Express Logo" />
+                    </div>
+                    <img src={PSQLLogo} alt="PSQL Logo" />
+                  </div>
+                </div>
+                <div className="App__Skills-Testing animate__animated animate__zoomIn card">
+                  <h2>
+                    <TypingText text={"Testing"} time={1000} delay={0} />
+                  </h2>
+                  <img src={JestLogo} alt="Jest Logo" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <Button delay={4000} content={"Projects"} link={"#projects"} />
+        </>
+      ) : null}
+    </section>
+  );
+};
+
+export default Skills;
